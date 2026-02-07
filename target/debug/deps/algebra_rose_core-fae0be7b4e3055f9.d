89ad8c77ca9248ecc2c714bb421f69325a0b6d54
IZA <15julio765@gmail.com>
2026-02-07 15:13:16 +0000

    al final

---
+++ target/debug/deps/algebra_rose_core-fae0be7b4e3055f9.d
@@ -0,0 +1,11 @@
+/workspaces/AR_app/target/debug/deps/algebra_rose_core-fae0be7b4e3055f9.d: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+
+/workspaces/AR_app/target/debug/deps/algebra_rose_core-fae0be7b4e3055f9: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+
+CORE/src/lib.rs:
+CORE/src/matrix_444.rs:
+CORE/src/algebra_griess.rs:
+CORE/src/love_operator.rs:
+CORE/src/keygen_evolution.rs:
+CORE/src/fibonacci_dimensions.rs:
+CORE/src/phi_constants.rs:
