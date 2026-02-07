9de62e322c7a68e1921a0183e53fdac87287742c
IZA <15julio765@gmail.com>
2026-02-07 15:26:51 +0000

    falla

---
+++ target/debug/deps/algebra_rose_interface-658db7e096bac153.d
@@ -0,0 +1,5 @@
+/workspaces/AR_app/target/debug/deps/algebra_rose_interface-658db7e096bac153.d: INTERFACE/src/main.rs
+
+/workspaces/AR_app/target/debug/deps/algebra_rose_interface-658db7e096bac153: INTERFACE/src/main.rs
+
+INTERFACE/src/main.rs:
---
+++ target/debug/deps/algebra_rose_interface-91092cd3c9658431.d
@@ -0,0 +1,7 @@
+/workspaces/AR_app/target/debug/deps/algebra_rose_interface-91092cd3c9658431.d: INTERFACE/src/lib.rs
+
+/workspaces/AR_app/target/debug/deps/libalgebra_rose_interface-91092cd3c9658431.rlib: INTERFACE/src/lib.rs
+
+/workspaces/AR_app/target/debug/deps/libalgebra_rose_interface-91092cd3c9658431.rmeta: INTERFACE/src/lib.rs
+
+INTERFACE/src/lib.rs:
