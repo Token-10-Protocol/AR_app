ee44a241187824b1b24e1ca97d04214df3086b36
IZA <15julio765@gmail.com>
2026-02-09 20:38:33 +0000

    built

---
+++ target/release/deps/algebra_rose_interface-2b9b4629b5356aac.d
@@ -0,0 +1,7 @@
+/workspaces/AR_app/target/release/deps/algebra_rose_interface-2b9b4629b5356aac.d: INTERFACE/src/lib.rs
+
+/workspaces/AR_app/target/release/deps/libalgebra_rose_interface-2b9b4629b5356aac.rlib: INTERFACE/src/lib.rs
+
+/workspaces/AR_app/target/release/deps/libalgebra_rose_interface-2b9b4629b5356aac.rmeta: INTERFACE/src/lib.rs
+
+INTERFACE/src/lib.rs:
---
+++ target/release/deps/algebra_rose_interface-b6733c543cb1d854.d
@@ -0,0 +1,5 @@
+/workspaces/AR_app/target/release/deps/algebra_rose_interface-b6733c543cb1d854.d: INTERFACE/src/main.rs
+
+/workspaces/AR_app/target/release/deps/algebra_rose_interface-b6733c543cb1d854: INTERFACE/src/main.rs
+
+INTERFACE/src/main.rs:
