06f9ce0cc99edd19311bec7d9b8519900e286836
IZA <15julio765@gmail.com>
2026-02-11 03:19:12 +0000

    test multidioma

---
+++ LINGUISTICS_MONSTER/target/debug/deps/algebra_rose_core-b05684867d31115e.d
@@ -0,0 +1,13 @@
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/algebra_rose_core-b05684867d31115e.d: /workspaces/AR_app/CORE/src/lib.rs /workspaces/AR_app/CORE/src/matrix_444.rs /workspaces/AR_app/CORE/src/algebra_griess.rs /workspaces/AR_app/CORE/src/love_operator.rs /workspaces/AR_app/CORE/src/keygen_evolution.rs /workspaces/AR_app/CORE/src/fibonacci_dimensions.rs /workspaces/AR_app/CORE/src/phi_constants.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/libalgebra_rose_core-b05684867d31115e.rlib: /workspaces/AR_app/CORE/src/lib.rs /workspaces/AR_app/CORE/src/matrix_444.rs /workspaces/AR_app/CORE/src/algebra_griess.rs /workspaces/AR_app/CORE/src/love_operator.rs /workspaces/AR_app/CORE/src/keygen_evolution.rs /workspaces/AR_app/CORE/src/fibonacci_dimensions.rs /workspaces/AR_app/CORE/src/phi_constants.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/libalgebra_rose_core-b05684867d31115e.rmeta: /workspaces/AR_app/CORE/src/lib.rs /workspaces/AR_app/CORE/src/matrix_444.rs /workspaces/AR_app/CORE/src/algebra_griess.rs /workspaces/AR_app/CORE/src/love_operator.rs /workspaces/AR_app/CORE/src/keygen_evolution.rs /workspaces/AR_app/CORE/src/fibonacci_dimensions.rs /workspaces/AR_app/CORE/src/phi_constants.rs
+
+/workspaces/AR_app/CORE/src/lib.rs:
+/workspaces/AR_app/CORE/src/matrix_444.rs:
+/workspaces/AR_app/CORE/src/algebra_griess.rs:
+/workspaces/AR_app/CORE/src/love_operator.rs:
+/workspaces/AR_app/CORE/src/keygen_evolution.rs:
+/workspaces/AR_app/CORE/src/fibonacci_dimensions.rs:
+/workspaces/AR_app/CORE/src/phi_constants.rs:
---
+++ LINGUISTICS_MONSTER/target/debug/deps/linguistics_monster-d1cc7505423c4b82.d
@@ -0,0 +1,6 @@
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/linguistics_monster-d1cc7505423c4b82.d: src/lib.rs src/monster_structures.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/linguistics_monster-d1cc7505423c4b82: src/lib.rs src/monster_structures.rs
+
+src/lib.rs:
+src/monster_structures.rs:
