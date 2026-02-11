c3d1065f6177059eacc73b4f130a919a731d79be
IZA <15julio765@gmail.com>
2026-02-11 03:15:05 +0000

    complilando falla

---
+++ LINGUISTICS_MONSTER/target/debug/deps/linguistics_monster-46429354e69de447.d
@@ -0,0 +1,8 @@
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/linguistics_monster-46429354e69de447.d: src/lib.rs src/monster_structures.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/liblinguistics_monster-46429354e69de447.rlib: src/lib.rs src/monster_structures.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/liblinguistics_monster-46429354e69de447.rmeta: src/lib.rs src/monster_structures.rs
+
+src/lib.rs:
+src/monster_structures.rs:
