dcaccf931b315cc77b2c50a8056a87d40aa1164d
IZA <15julio765@gmail.com>
2026-02-11 03:17:04 +0000

    limpieza

--- LINGUISTICS_MONSTER/target/debug/build/typenum-02159cc3af3ecb58/build_script_build-02159cc3af3ecb58.d
+++
@@ -1,5 +0,0 @@
-/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/build/typenum-02159cc3af3ecb58/build_script_build-02159cc3af3ecb58.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/typenum-1.19.0/build.rs
-
-/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/build/typenum-02159cc3af3ecb58/build_script_build-02159cc3af3ecb58: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/typenum-1.19.0/build.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/typenum-1.19.0/build.rs:
--- LINGUISTICS_MONSTER/target/debug/deps/rawpointer-f174ef2bf2075b70.d
+++
@@ -1,7 +0,0 @@
-/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/rawpointer-f174ef2bf2075b70.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/librawpointer-f174ef2bf2075b70.rlib: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/librawpointer-f174ef2bf2075b70.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs:
