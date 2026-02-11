96e2ca3f79e5e381ed70be65011dd3eb44229ba0
IZA <15julio765@gmail.com>
2026-02-11 02:01:58 +0000

    test lengua falla

---
+++ LINGUISTICS_MONSTER/target/debug/deps/algebra_rose_core-e28c471932b3dfbf.d
@@ -0,0 +1,11 @@
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/algebra_rose_core-e28c471932b3dfbf.d: /workspaces/AR_app/CORE/src/lib.rs /workspaces/AR_app/CORE/src/matrix_444.rs /workspaces/AR_app/CORE/src/algebra_griess.rs /workspaces/AR_app/CORE/src/love_operator.rs /workspaces/AR_app/CORE/src/keygen_evolution.rs /workspaces/AR_app/CORE/src/fibonacci_dimensions.rs /workspaces/AR_app/CORE/src/phi_constants.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/libalgebra_rose_core-e28c471932b3dfbf.rmeta: /workspaces/AR_app/CORE/src/lib.rs /workspaces/AR_app/CORE/src/matrix_444.rs /workspaces/AR_app/CORE/src/algebra_griess.rs /workspaces/AR_app/CORE/src/love_operator.rs /workspaces/AR_app/CORE/src/keygen_evolution.rs /workspaces/AR_app/CORE/src/fibonacci_dimensions.rs /workspaces/AR_app/CORE/src/phi_constants.rs
+
+/workspaces/AR_app/CORE/src/lib.rs:
+/workspaces/AR_app/CORE/src/matrix_444.rs:
+/workspaces/AR_app/CORE/src/algebra_griess.rs:
+/workspaces/AR_app/CORE/src/love_operator.rs:
+/workspaces/AR_app/CORE/src/keygen_evolution.rs:
+/workspaces/AR_app/CORE/src/fibonacci_dimensions.rs:
+/workspaces/AR_app/CORE/src/phi_constants.rs:
---
+++ LINGUISTICS_MONSTER/target/debug/deps/approx-798ca4264ea34cff.d
@@ -0,0 +1,9 @@
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/approx-798ca4264ea34cff.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/abs_diff_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/relative_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/ulps_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/macros.rs
+
+/workspaces/AR_app/LINGUISTICS_MONSTER/target/debug/deps/libapprox-798ca4264ea34cff.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/abs_diff_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/relative_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/ulps_eq.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/macros.rs
+
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/lib.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/abs_diff_eq.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/relative_eq.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/ulps_eq.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/approx-0.5.1/src/macros.rs:
