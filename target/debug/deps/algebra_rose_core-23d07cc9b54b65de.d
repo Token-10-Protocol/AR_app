422b8cf1b0f85b60a724cc1b2efe4ca08992941f
IZA <15julio765@gmail.com>
2026-02-11 20:01:52 +0000

    compoliando

--- target/debug/deps/algebra_rose_core-23d07cc9b54b65de.d
+++ target/debug/deps/algebra_rose_core-23d07cc9b54b65de.d
@@ -1,6 +1,6 @@
-/workspaces/AR_app/target/debug/deps/algebra_rose_core-23d07cc9b54b65de.d: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+/workspaces/AR_app/target/debug/deps/algebra_rose_core-23d07cc9b54b65de.d: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs CORE/src/conscious_operators.rs
 
-/workspaces/AR_app/target/debug/deps/algebra_rose_core-23d07cc9b54b65de: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+/workspaces/AR_app/target/debug/deps/algebra_rose_core-23d07cc9b54b65de: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs CORE/src/conscious_operators.rs
 
 CORE/src/lib.rs:
 CORE/src/matrix_444.rs:
@@ -9,3 +9,4 @@ CORE/src/love_operator.rs:
 CORE/src/keygen_evolution.rs:
 CORE/src/fibonacci_dimensions.rs:
 CORE/src/phi_constants.rs:
+CORE/src/conscious_operators.rs:
--- target/debug/deps/algebra_rose_core-e04df91a1b4b47e9.d
+++ target/debug/deps/algebra_rose_core-e04df91a1b4b47e9.d
@@ -1,8 +1,8 @@
-/workspaces/AR_app/target/debug/deps/algebra_rose_core-e04df91a1b4b47e9.d: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+/workspaces/AR_app/target/debug/deps/algebra_rose_core-e04df91a1b4b47e9.d: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs CORE/src/conscious_operators.rs
 
-/workspaces/AR_app/target/debug/deps/libalgebra_rose_core-e04df91a1b4b47e9.rlib: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+/workspaces/AR_app/target/debug/deps/libalgebra_rose_core-e04df91a1b4b47e9.rlib: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs CORE/src/conscious_operators.rs
 
-/workspaces/AR_app/target/debug/deps/libalgebra_rose_core-e04df91a1b4b47e9.rmeta: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs
+/workspaces/AR_app/target/debug/deps/libalgebra_rose_core-e04df91a1b4b47e9.rmeta: CORE/src/lib.rs CORE/src/matrix_444.rs CORE/src/algebra_griess.rs CORE/src/love_operator.rs CORE/src/keygen_evolution.rs CORE/src/fibonacci_dimensions.rs CORE/src/phi_constants.rs CORE/src/conscious_operators.rs
 
 CORE/src/lib.rs:
 CORE/src/matrix_444.rs:
@@ -11,3 +11,4 @@ CORE/src/love_operator.rs:
 CORE/src/keygen_evolution.rs:
 CORE/src/fibonacci_dimensions.rs:
 CORE/src/phi_constants.rs:
+CORE/src/conscious_operators.rs:
